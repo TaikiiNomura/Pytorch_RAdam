97a1fdb062753dff563eb18a8bd13609a65a8050
TaikiiNomura <tk.nomua@gmail.com>
2026-05-04 23:55:19 +0900

    Update radam.py

--- radam.py
+++ radam.py
@@ -27,11 +27,6 @@ class MyRAdam(torch.optim.Optimizer):
                 loss = closure()
 
         for group in self.param_groups:
-            params = []
-            grads = []
-            exp_avgs = []
-            exp_avg_sqs = []
-            steps = []
 
             beta1, beta2 = group["betas"]
             lr = group["lr"]
@@ -44,102 +39,63 @@ class MyRAdam(torch.optim.Optimizer):
                     continue
                 if p.grad.is_sparse:
                     raise RuntimeError("RAdam does not support sparse gradients")
+                
+                grad = p.grad
 
                 state = self.state[p]
 
                 if len(state) == 0:
-                    state["step"] = torch.tensor(0.0, device=p.device)
+                    state["step"] = 0
                     state["exp_avg"] = torch.zeros_like(p)
                     state["exp_avg_sq"] = torch.zeros_like(p)
+                
+                exp_avg = state["exp_avg"]
+                exp_avg_sq = state["exp_avg_sq"]
 
-                params.append(p)
-                grads.append(p.grad)
-                exp_avgs.append(state["exp_avg"])
-                exp_avg_sqs.append(state["exp_avg_sq"])
-                steps.append(state["step"])
+                state["step"] += 1
+                step = state["step"]
 
-            # --- update ---
-            _single_radam_step(
-                params,
-                grads,
-                exp_avgs,
-                exp_avg_sqs,
-                steps,
-                weight_decay,
-                decoupled_weight_decay,
-                lr,
-                beta1,
-                beta2,
-                eps,
-            )
+                if weight_decay != 0:
+                    if decoupled_weight_decay:
+                        p.mul_(1 - lr * weight_decay)
+                    else:
+                        grad = grad.add(p, alpha=weight_decay)
+                
+                # exp_avg.mul_(beta1).add_(grad, alpha=1 - beta1)
+                exp_avg.lerp_(grad, 1 - beta1)
+                exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
+
+                beta1_t = beta1 ** step
+                beta2_t = beta2 ** step
+                bias_correction1 = 1 - beta1_t
+                bias_correction2 = 1 - beta2_t
+
+                bias_corrected_exp_avg = exp_avg / bias_correction1
+
+                rho_inf = 2 / (1 - beta2) - 1
+                rho_t = rho_inf - 2 * step * (beta2_t) / bias_correction2
+
+                if rho_t > 5:
+                    rect = (
+                        (rho_t - 4)
+                        * (rho_t - 2)
+                        * rho_inf
+                        / ((rho_inf - 4) * (rho_inf - 2) * rho_t)
+                    ) ** 0.5
+
+                    exp_avg_sq_sqrt = exp_avg_sq.sqrt()
+                    exp_avg_sq_sqrt = exp_avg_sq_sqrt.add_(eps)
+
+                    adaptive_lr = (bias_correction2**0.5) / exp_avg_sq_sqrt
+
+                    p.add_(
+                        bias_corrected_exp_avg
+                        * lr
+                        * adaptive_lr
+                        * rect,
+                        alpha=-1.0,
+                    )
+                else:
+                    p.add_(bias_corrected_exp_avg * lr, alpha=-1.0)
 
         return loss
-
-def _single_radam_step(
-    params,
-    grads,
-    exp_avgs,
-    exp_avg_sqs,
-    steps,
-    weight_decay,
-    decoupled_weight_decay,
-    lr,
-    beta1,
-    beta2,
-    eps,
-):
-    for i, p in enumerate(params):
-        grad = grads[i]
-        exp_avg = exp_avgs[i]
-        exp_avg_sq = exp_avg_sqs[i]
-
-        # --- step ---
-        steps[i].add_(1)
-        step = steps[i] # tensor
-        # step = steps[i].item() # scaler
-        # step = float(steps[i]) # mps
-
-        # --- weight decay ---
-        if weight_decay != 0:
-            if decoupled_weight_decay:
-                p.mul_(1 - lr * weight_decay)
-            else:
-                grad = grad.add(p, alpha=weight_decay)
-
-        # 移動平均
-        exp_avg.mul_(beta1).add_(grad, alpha=1 - beta1)
-        exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
-
-        # --- bias correction ---
-        bias_correction1 = 1 - beta1 ** step
-        bias_correction2 = 1 - beta2 ** step
-
-        # 自由度を計算
-        rho_inf = 2 / (1 - beta2) - 1
-        rho_t = rho_inf - 2 * step * (beta2**step) / bias_correction2
-
-        if rho_t > 4:
-            # 補正項付きAdam
-            # p -= lr * rect * m_hat / {sqrt(v_hat) + eps}
-            rect = (
-                (rho_t - 4)
-                * (rho_t - 2)
-                * rho_inf
-                / ((rho_inf - 4) * (rho_inf - 2) * rho_t)
-            ) ** 0.5
-
-            bias_correction2_sqrt = bias_correction2 ** 0.5
-
-            # denom = exp_avg_sq.sqrt().div_(bias_correction2_sqrt)
-            # denom.add_(eps)
-            denom = exp_avg_sq.sqrt().div(bias_correction2_sqrt).add_(eps)
-
-            step_size = lr * rect / bias_correction1
-
-            p.addcdiv_(exp_avg, denom, value=-step_size)
-
-        else:
-            # モーメントつきSGD
-            # p -= lr * m_hat
-            step_size = lr / bias_correction1
-            p.add_(exp_avg, alpha=-step_size)
